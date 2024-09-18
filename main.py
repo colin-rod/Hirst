6c911b719a3c3d93e85a78f8bebaedb946bd526e
Colin Rodrigues <colin.rods@gmail.com>
2024-09-18 15:07:47 +0200

    commit

--- main.py
+++ main.py
@@ -22,14 +22,24 @@ timmy.shape("turtle")
 #         timmy.right(360/i)
 #     timmy.pencolor(randint(0, 100) / 100,randint(0, 100) / 100,randint(0, 100) / 100)
 
-timmy.speed(10)
-timmy.pensize(5)
-print(randint(1,4))
-for i in range(200):
-    timmy.forward(20)
-    angles=[-90,0,90]
-    timmy.right(random.choice(angles))
+timmy.speed(0)
+# timmy.pensize(5)
+# print(randint(1,4))
+# for i in range(200):
+#     timmy.forward(20)
+#     angles=[-90,0,90]
+#     timmy.right(random.choice(angles))
+#     timmy.pencolor(randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100)
+
+steps = 50
+angle_step = 360/steps
+angle = 0
+for i in range(steps):
+    timmy.setheading(angle)
     timmy.pencolor(randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100)
+    timmy.circle(100)
+    angle += angle_step
+    print(i)
 
 
 screen = Screen()
