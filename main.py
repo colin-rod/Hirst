af770ae28c27da4d424739df42b8d20de5f247b6
Colin Rodrigues <colin.rods@gmail.com>
2024-09-20 11:48:33 +0200

    commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from random import randint
 from turtle import Turtle, Screen
 import random
+import colorgram
 
 timmy = Turtle()
-timmy.shape("turtle")
+timmy.shape("circle")
 
 # for i in range(4):
 #     timmy.forward(100)
@@ -22,7 +23,7 @@ timmy.shape("turtle")
 #         timmy.right(360/i)
 #     timmy.pencolor(randint(0, 100) / 100,randint(0, 100) / 100,randint(0, 100) / 100)
 
-timmy.speed(0)
+# timmy.speed(0)
 # timmy.pensize(5)
 # print(randint(1,4))
 # for i in range(200):
@@ -31,15 +32,48 @@ timmy.speed(0)
 #     timmy.right(random.choice(angles))
 #     timmy.pencolor(randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100)
 
-steps = 50
-angle_step = 360/steps
-angle = 0
-for i in range(steps):
-    timmy.setheading(angle)
-    timmy.pencolor(randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100)
-    timmy.circle(100)
-    angle += angle_step
-    print(i)
+# steps = 50
+# angle_step = 360/steps
+# angle = 0
+# for i in range(steps):
+#     timmy.setheading(angle)
+#     timmy.pencolor(randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100)
+#     timmy.circle(100)
+#     angle += angle_step
+#     print(i)
+
+number = 30
+colors = colorgram.extract('image.jpg', number)
+print(colors)
+color_list= []
+
+for i in range(2,number):
+    first_color = colors[i]
+    r = first_color.rgb.r/255
+    g = first_color.rgb.g/255
+    b = first_color.rgb.b/255
+    rgb = (r,g,b)
+    color_list.append(rgb)
+
+print(color_list)
+dots = 10
+space = 50
+size = 20
+
+timmy.pensize(size)
+x= -1*((space*(dots-1))/2)
+y = x
+timmy.teleport(x,y)
+for i in range(dots):
+    for j in range(dots):
+        timmy.pencolor(random.choice(color_list))
+        timmy.dot(10)
+        timmy.penup()
+        timmy.forward(space)
+    y += space
+    timmy.teleport(x,y)
+
+
 
 
 screen = Screen()
